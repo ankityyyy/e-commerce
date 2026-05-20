4137aa53ed890d3f1fe67c0307a594e7c0a2f114
ankityyyy <ankitdubey9431969639@gmail.com>
2026-05-20 09:00:47 +0530

    fixed product casing

--- backend/src/middleware/productMill.js
+++ backend/src/middleware/productMill.js
@@ -1,4 +1,4 @@
-import Product from "../models/product.js";
+import Product from "../models/Product.js";
 import ExpressError from "../utils/ExpressError.js";
 import {StatusCodes} from "http-status-codes"
 import wrapAsync from "../utils/wrapAsync.js";
